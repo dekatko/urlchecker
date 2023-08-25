4210903c274f9ffcda787f3989a1cf2a80f6c417
Denis <dekatko@gmail.com>
2023-08-25 15:13:19 +0200

    First

---
+++ src/main/java/de/urlchecker/urlchecker/UrlcheckerApplication.java
@@ -0,0 +1,13 @@
+package de.urlchecker.urlchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlcheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlcheckerApplication.class, args);
+	}
+
+}
